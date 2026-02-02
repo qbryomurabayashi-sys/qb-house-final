ee18653368496dcfa184587c52703741ee477cdb
qbryomurabayashi-sys <qb.ryo.murabayashi@gmail.com>
2026-02-02 23:02:57 +0900

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-// import App from './App.tsx' // Disabled for V6 Debug
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <div style={{ padding: '50px', textAlign: 'center' }}>
-            <h1 style={{ color: 'green', fontSize: '24px' }}>V6: System Check OK</h1>
-            <p>Basic React rendering is working.</p>
-            <p>Illegal Constructor Error Debugging...</p>
-        </div>
+        <App />
     </React.StrictMode>,
 )
